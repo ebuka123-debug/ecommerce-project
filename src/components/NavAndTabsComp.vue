edbc7046484eb97f9483f778be7b0db39bb30f79
unknown <ichukwuebuka26@gmail.com>
2026-02-25 14:58:32 +0100

    Created an isActiveRoute method that check if the route path is current or active

--- src/components/NavAndTabsComp.vue
+++ src/components/NavAndTabsComp.vue
@@ -1,27 +1,36 @@
 <script setup>
+import { useRoute } from 'vue-router';
+
+
+//Checks if the the route path is the current path (The page the user is in is equivalent to the route path)
+const isActiveRoute = (routePath) => {
+  const route = useRoute(routePath);
+
+  return route.path === routePath
+}
 </script>
 
 <template>
   <div class="nav-tabs d-none pb-2 border-bottom-5 mt-4 d-md-flex justify-content-center align-items-center">
       <div id="nav-tabs" class="d-flex align-items-center">
           <div class="me-4">
-              <!-- <b><a href="../index.html" >Home</a></b> -->
-              <b><RouterLink to="/" class="active-link">Home</RouterLink></b>
+              <!-- <b><a to="../index.html" >Home</RouterLink></b> -->
+              <b><RouterLink to="/" :class="isActiveRoute('/')? 'active-link':''">Home</RouterLink></b>
           </div>
           <div class="me-4">
-              <b><a href="">Hot deals</a></b>
+              <b><RouterLink to="">Hot deals</RouterLink></b>
           </div>
           <div class="me-4">
-              <b><a href="">Categories</a></b>
+              <b><RouterLink to="">Categories</RouterLink></b>
           </div>
           <div class="me-4">
-              <b><a href="">Laptops</a></b>
+              <b><RouterLink to="">Laptops</RouterLink></b>
           </div>
           <div class="me-4">
-              <b><a href="">Cameras</a></b>
+              <b><RouterLink to="">Cameras</RouterLink></b>
           </div>
           <div class="me-4">
-              <b><a href="">Cameras</a></b>
+              <b><RouterLink to="">Cameras</RouterLink></b>
           </div>
       </div>
   </div>
