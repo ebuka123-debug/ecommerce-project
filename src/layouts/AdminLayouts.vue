e0eab7c1b041c6534b29b139bd1f75434c17f6a2
unknown <ichukwuebuka26@gmail.com>
2026-04-07 15:59:40 +0100

    Created an admin layout file

---
+++ src/layouts/AdminLayouts.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import '@/assets/styles/admin-utilities.css'
+import AdminSidebar from '@/components/adminComponents/AdminSidebar.vue';
+import Navbar from '@/components/adminComponents/Navbar.vue';
+
+</script>
+
+
+
+<template>
+  <div class="admin-layout">
+    <AdminSidebar />  <!-- admins sidebar -->
+    <!-- <main> -->
+      <div class="main-content">
+        <Navbar />
+        <div class="content-area">
+          <RouterView />
+        </div>
+      </div>
+    <!-- <main> -->
+  </div>
+</template>
+
+<!-- <script setup>
+import AdminSidebar from '@/components/AdminSidebar.vue'
+</script> -->
