77c67a96952aefd6cd31fa2702e9e13363ff7ea0
unknown <ichukwuebuka26@gmail.com>
2026-04-08 14:43:22 +0100

    Created an inventory managemnt card

---
+++ src/components/adminComponents/InventoryManagementCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: String,
+  content: String,
+  textColor: String
+})
+</script>
+<template>
+  <div class="card">
+      <div class="card-body">
+          <h6 class="text-muted">{{ title }}</h6>
+          <h3 :class="[textColor]">{{ content }}</h3>
+      </div>
+  </div>
+</template>
