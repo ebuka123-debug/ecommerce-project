847cc621c80319d6250cd5f8b51644a4cb1b848b
unknown <ichukwuebuka26@gmail.com>
2026-04-08 15:24:36 +0100

    Added a period card component

---
+++ src/components/adminComponents/PeriodAnalyticsCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  period: String,
+  content: String,
+  percentage: String
+
+})
+</script>
+
+<template>
+  <div class="col-md-3">
+    <div class="text-center p-3 border rounded">
+        <h6 class="text-muted">{{ period }}</h6>
+        <h4>{{ content }}</h4>
+        <small class="text-success">{{ percentage }}</small>
+    </div>
+  </div>
+</template>
