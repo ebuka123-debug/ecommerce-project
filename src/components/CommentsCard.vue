19e3b847bda221de9b00e032705625196f05bdcc
unknown <ichukwuebuka26@gmail.com>
2026-04-17 19:58:14 +0100

    Added some properties to the comment card component

--- src/components/CommentsCard.vue
+++ src/components/CommentsCard.vue
@@ -1,14 +1,12 @@
 <script setup>
-
+const props = defineProps({
+  username: String,
+  comment: String,
+  date: String
+})
 </script>
 
 <template>
-  <div class="col-md-6 col-xl-8 comment-section">
-    <div class="mt-5 mt-md-3 border-bottom pb-1">
-        <span>
-            COMMENTS FROM VERIFIED PURCHASES (1234)
-        </span>
-    </div>
     <div class="comment-box mb-3 border-bottom mt-3 pb-2">
 
         <div class="mb-2">
@@ -21,16 +19,22 @@
 
         <div class="comment-title mb-2">
             <b>
-                This is nice
+                {{ username }}
             </b>
         </div>
 
         <div class="comment-body fs-14">
-            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, nihil. Sed similique, nostrum consectetur labore nihil excepturi voluptatibus earum eaque sit velit recusandae atque numquam. Deleniti deserunt necessitatibus velit minima?</span>
+            <span>
+              {{ comment }}
+            </span>
         </div>
-        <div class="comment-footer mt-2 fs-14">
-            <span class="text-ash">22-11-2025 </span><span>by Chukwuebuka</span>
+        <div class="comment-footer mt-2 fs-14 d-flex">
+            <span class="text-ash me-2">{{ date }} </span><span class="text-ash"> Powered by</span>
+          <b class="fs-14 ms-1">
+              Specs
+          </b>
+          <div id="fullstop" class="rounded-circle ms-1 mt-2"></div>
         </div>
     </div>
-  </div>
+
 </template>
